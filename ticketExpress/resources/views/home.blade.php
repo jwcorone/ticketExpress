75e0b7cff280e1cc6c3b9023eac17e2e7a7164f6
Jose Coronel <jwcorone@espol.edu.ec>
2016-11-17 20:59:02 -0500

    Correccion de errores

--- ticketExpress/resources/views/home.blade.php
+++ ticketExpress/resources/views/home.blade.php
@@ -21,7 +21,7 @@
             </div>
         </div>
          <div>
-            <button type="submit" style="width:90%" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
+            <button type="submit" style="width:90%" class="btn btn-primary btn-block btn-flat">Reservar bus</button>
         </div><!-- /.col -->
     </div>
 </div>
