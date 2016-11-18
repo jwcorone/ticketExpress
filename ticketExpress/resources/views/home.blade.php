f443bdaf08f90b8898fb5f6f5eb2880be30f07e5
Jose Coronel <jwcorone@espol.edu.ec>
2016-11-17 20:53:41 -0500

    Diseño responsibe para el mapa principal

--- ticketExpress/resources/views/home.blade.php
+++ ticketExpress/resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('htmlheader_title')
-	Home
+	Bievenido
 @endsection
 
 
@@ -10,31 +10,81 @@
 <script async defer
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCzp4ZqXQC0xsYh93Dzyu6OJeLOPU3v2Uo&callback=initMap">
 </script>
+<div class="container">
+
     <div >
-        <div id="mapa" class="google-maps" >
-        	cargando mapa
+        <div >
+
+            <div >
+                <div id="mapa"  class="embed-container" style="width:90%;">       cargando mapa
+                </div>
+            </div>
         </div>
          <div>
-            <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
+            <button type="submit" style="width:90%" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
         </div><!-- /.col -->
     </div>
+</div>
     <div>
 
     </div>
         <style>
-            .google-maps {
-            position: relative;
-            padding-bottom: 85%; // This is the aspect ratio
-            height: 0;
-            overflow: hidden;
-            }
-            .google-maps iframe {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100% !important;
-            height: 100% !important;
-            }
+/* CSS general */
+.embed-container {
+    position: relative;
+    padding-bottom: 120%;
+    height: 0;
+    overflow: hidden;
+}
+.embed-container iframe {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+/* CSS pantallas de 320px o superior */
+@media (min-width: 520px) {
+
+  .embed-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    height: 0;
+    overflow: hidden;
+}
+.embed-container iframe {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+}
+
+/* CSS pantalla 768px o superior */
+@media (min-width: 768px) {
+
+  .embed-container {
+    position: relative;
+    padding-bottom: 40.25%;
+    height: 0;
+    overflow: hidden;
+}
+.embed-container iframe {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+}
+
+
+
+           
         </style>
         <script >
             var divmapa= document.getElementById('mapa');
@@ -61,6 +111,10 @@
 
                 
             }
+
+            
+
+         
 
         </script>
 @endsection
